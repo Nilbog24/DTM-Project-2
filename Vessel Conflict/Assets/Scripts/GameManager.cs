4a7b705c5dbd8ae9bb741a928001144601d7d2a9
Nilbog24 <19342@wc.school.nz>
2022-10-06 12:16:05 +1300

    Added Game Manager script and Tile Manager script

--- Vessel Conflict/Assets/Scripts/GameManager.cs
+++ Vessel Conflict/Assets/Scripts/GameManager.cs
@@ -4,41 +4,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // These two variables are used to determine which players turn it is.
-    // There are two variables's here instead of one that switches between two different states.
-    // This set up works better with the rest of the code.
-    public bool PlayerOneTurn = false; 
-    public bool PlayerTwoTurn = false;
-    // These two variables are used to determine the amount of ships remaining on each side.
-    public int PlayerOneShips;
-    public int PlayerTwoShips;
-    // This variable is used when determining if the player is alone or not.
-    public int GameMode;
-    // This is used when the game is in two player mdoe to determine who starts.
-    private int WhoStarts;
+    public GameObject[] ships;
+    private bool setupComplete = false;
+    private bool playerOneTurn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
 
     // Update is called once per frame
     void Update()
     {  
-        // If the the game is in twoplayer mode then this will run
-        if(GameMode == 2){
-            // This will reset the number of ships on both side to five
-            PlayerOneShips = 5;
-            PlayerTwoShips = 5;
-            // This will decide who starts
-            WhoStarts = Random.Range(1,2);
-            //There will be code here to trigger a coin flip animation later.
-
-            if (WhoStarts == 1){
-                PlayerOneTurn = true;
-            }
-
-            if (WhoStarts == 2){
-                PlayerTwoTurn = true;
-            }
-
-        }
-            
-           
+  
     }
+
+    public void TileClicked(GameObject tile)
+    {
+        if(setupComplete && playerOneTurn)
+        {
+            // SHooting?
+        }
+        else if(!setupComplete)
+        {
+            PlaceShip(tile);
+        }
+    }
+
+    private void PlaceShip(GameObject tile)
+    {}
 }
---
+++ Vessel Conflict/Assets/Scripts/TileScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    GameManager gameManager;
+    Ray ray;
+    RaycastHit hit;
+
+    private bool missileHit = false;
+    Color32[] hitColor = new Color32[2];
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<gameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(Input.GetMouseButtonDown(0) && hit.collider.gameObject.name == gameObject.name)
+            {
+                if(missileHit == false)
+                {
+                    gameManager.TileClicked(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
